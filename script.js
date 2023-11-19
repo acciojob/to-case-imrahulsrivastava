adf2de44ac5873310de99949f9118899db14e143
Rahul Kumar Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2023-11-19 20:35:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function toCase(text) {
   // write your code here
+
+  if (text === null || text.trim() === "") {
+    return "";
+  }
+
+  const filteredText = text.split(" ").join("").toLowerCase();
+  const result = `${filteredText}-${filteredText.toUpperCase()}`;
+
+  return result;
 }
 
 // DO not change the code below
